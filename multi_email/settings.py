f13a3057ee2c0868e0060738e676e98be08a4469
Akande Al-ameen <longlasting1805@gmail.com>
2021-12-01 14:53:16 +0000

    settings

--- multi_email/settings.py
+++ multi_email/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
                     ]
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
